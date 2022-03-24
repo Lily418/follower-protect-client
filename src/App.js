9b58d6ca6eafdada04dc3d774e727c44295d679a
Lily <1368025+Lily418@users.noreply.github.com>
2022-03-24 01:46:37 +0000

    Add bio

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import parse from "html-react-parser";
 function App() {
 
   const [users, setUsers] = useState([])
-  const [showPost, setShowPost] = useState(null) 
+  const [showPost, setShowPost] = useState(null)
 
   useEffect(() => {
     (async () => {
@@ -20,21 +20,34 @@ function App() {
   return (
     <div className="App">
       <h1>Follower Protect</h1>
-      <div style={{"display": "flex", "flexDirection": "row"}}>
-      <table>
-        <tbody>
-        {users && users.map((user, index) => <tr key={user._id}><td><a href={user._id}>{user.user.name}</a></td><td style={user.posts.length > 0 ? {"color": "red", "fontWeight": "bold", "width": "200px"} : {}}>{user.posts.length}</td><td><a href='#' onClick={() => {showPost === index ? setShowPost(null) : setShowPost(index)}}>Show Posts</a></td></tr>)}
-      </tbody>
-      </table>
+      <div style={{ "display": "flex", "flexDirection": "row" }}>
+        <table>
+          <thead>
+            <th>Name</th>
+            <th>Bio</th>
+            <th>Flagged Posts</th>
+          </thead>
+          <tbody>
 
-      <div style={{"flex": 1}}>
-        <h2>{showPost !== null ? users[showPost].user.name : ""}</h2>
-        {showPost !== null ? users[showPost].posts.map((post) => {
-          return (
-            parse(post)
-          )
-        }) : null}
-      </div>
+            {users && users.map((user, index) => {
+              return (<tr key={user._id}>
+                <td style={{"verticalAlign": "top"}}><a href={user._id}>{user.user.name}</a></td>
+                <td style={{"textAlign": "start"}}>{parse(user.bio)}</td>
+                <td style={user.posts.length > 0 ? { "verticalAlign": "top", "color": "red", "fontWeight": "bold", "width": "200px" } : {"verticalAlign": "top"}}>{user.posts.length}</td>
+                <td style={{"verticalAlign": "top"}}><a href='#' onClick={() => { showPost === index ? setShowPost(null) : setShowPost(index) }}>Show Posts</a></td>
+              </tr>)
+            })}
+          </tbody>
+        </table>
+
+        <div style={{ "flex": 1 }}>
+          <h2>{showPost !== null ? users[showPost].user.name : ""}</h2>
+          {showPost !== null ? users[showPost].posts.map((post) => {
+            return (
+              parse(post)
+            )
+          }) : null}
+        </div>
       </div>
     </div>
   );
