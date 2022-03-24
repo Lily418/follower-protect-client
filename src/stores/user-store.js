7a92f24d84384d1bc654fc2adfb41b70e7fdcc6b
Lily <1368025+Lily418@users.noreply.github.com>
2022-03-24 08:49:37 +0000

    Sort by date

--- src/stores/user-store.js
+++ src/stores/user-store.js
@@ -4,5 +4,5 @@ const API = process.env.REACT_APP_TUMBLR_ENDPOINT
 
 export const getUsers = async () => {
     const users = await axios.get(`${API}`)
-    return users.data.sort((a, b) => a.posts.length < b.posts.length)
+    return users.data.sort((a, b) => a.posts.length === b.posts.length ? a.date < b.date : a.posts.length < b.posts.length)
 }
